3215572205f9495f7877301aca84d53b94fe049f
Erwin Müller <erwin.mueller@deventm.org>
2018-09-06 18:18:16 +0200

    Use project prefix.

--- image/rootfs/setup-ssh.sh
+++ image/rootfs/setup-ssh.sh
@@ -2,24 +2,24 @@
 set -e
 
 function setup_known_host() {
-    ssh-keyscan ${SSH_HOST} >> ${SSH_HOME}/known_hosts
+    ssh-keyscan ${PROJECT_SSH_HOST} >> ${PROJECT_SSH_HOME}/known_hosts
 }
 
 function setup_ssh() {
-    cp ${SSH_FILE} ${SSH_HOME}/id_rsa
-    chmod go-rw ${SSH_HOME}/id_rsa
+    echo "${PROJECT_SSH_ID_RSA}" > ${PROJECT_SSH_HOME}/id_rsa
+    chmod go-rw ${PROJECT_SSH_HOME}/id_rsa
 }
 
-source /docker-entrypoint-utils.sh
-set_debug
+#source /docker-entrypoint-utils.sh
+#set_debug
 echo "Running as `id`"
 
 needed_env=(
-SSH_HOME
-SSH_HOST
-SSH_FILE
-SSH_USER
-SSH_PASS
+PROJECT_SSH_HOME
+PROJECT_SSH_HOST
+PROJECT_SSH_ID_RSA
+PROJECT_SSH_USER
+PROJECT_SSH_PASS
 )
 
 for e in "${needed_env[@]}"; do
