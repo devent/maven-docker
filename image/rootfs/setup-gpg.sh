47643b76f7b0452c56f0e0acefe87544ce5280c2
Erwin Müller <erwin.mueller@deventm.org>
2018-09-06 08:38:46 +0200

    Initial commit.

---
+++ image/rootfs/setup-gpg.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+function setup_gpg() {
+    mkdir ~/.gnupg
+    chmod 700 ~/.gnupg
+    echo "use-agent" >> ~/.gnupg/gpg.conf
+    echo "pinentry-mode loopback" >> ~/.gnupg/gpg.conf
+    echo "allow-loopback-pinentry" >> ~/.gnupg/gpg-agent.conf
+    echo "allow-preset-passphrase" >> ~/.gnupg/gpg-agent.conf
+    echo RELOADAGENT | gpg-connect-agent
+    echo "${GPG_PASSPHRASE}" | base64 -d | gpg --passphrase-fd 0 --allow-secret-key-import --import ${GPG_KEY_FILE}
+}
+
+function sign_cache_gpg() {
+    echo "document" > doc
+    echo "${GPG_PASSPHRASE}" | base64 -d | gpg --no-tty --batch --passphrase-fd 0 --output doc.sig --sign doc
+}
+
+source /docker-entrypoint-utils.sh
+set_debug
+echo "Running as `id`"
+
+if [[ -z "${GPG_PASSPHRASE}" ]]; then
+    echo "No GPG_PASSPHRASE set."
+fi
+
+if [[ -z "${GPG_KEY_FILE}" ]]; then
+    echo "No GPG_KEY_FILE set."
+fi
+
+setup_gpg
+sign_cache_gpg
