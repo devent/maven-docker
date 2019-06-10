7c76c6287bbe70abc274cce399dc0497fbff37b2
Erwin Müller <erwin.mueller@deventm.org>
2019-06-10 18:07:51 +0200

    Remove chown
    It slows down the pipeline and is not needed for the build.

--- image/rootfs/setup.sh
+++ image/rootfs/setup.sh
@@ -3,8 +3,3 @@ set -e
 
 source /docker-entrypoint-utils.sh
 set_debug
-echo "Running as `id`"
-
-if [[ "`id -u`" = "0" ]]; then
-    chown ${PROJECT_JENKINS_USER}.root -R ${PROJECT_JENKINS_WORKSPACE}
-fi
